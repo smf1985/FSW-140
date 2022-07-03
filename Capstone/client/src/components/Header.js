de7c4c33986b7a4c2d0c2f97b2039129c1c6a397
Stephanie Freeman <stephanie.freeman@bryanuniversity.edu>
2022-07-03 14:39:32 -0700

    added/updated assignment

---
+++ Capstone/client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <img src="https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHdvcmt8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" width="100%" height="450px" />
+            <h1 className="flexoutput">Todo List</h1>
+        </div>
+    )
+}
+export default Header
